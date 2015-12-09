65330d63992fa2f8696308a06991cb9b031668b1
mudragada <chaitanyamudragada@gmail.com>
2015-12-09 15:04:04 -0500

    Update curlCallee.py

--- pythonworks/Curls/Curls/curlCallee.py
+++ pythonworks/Curls/Curls/curlCallee.py
@@ -1,12 +1,10 @@
-__author__ = 'Krishna Mudragada'
-
 from curlRequester import sendCurlRequest
 
-username = 'krishna'
-password = 'Eagles@123'
+username = 'admin'
+password = 'password'
 instancesFile = 'instancesspreadout'
 def BrowsePageServiceFlush():
-    path = 'aeo/commerce/catalog/services/BrowsePageService/?shouldInvokeMethod=flushAllBrowseCaches'
+    path = 'url/?shouldInvokeMethod=flushAllBrowseCaches'
     typeList = ['831','832','834']
     curlOnSocketsFromFile(instancesFile, path , username, password, typeList)
 
