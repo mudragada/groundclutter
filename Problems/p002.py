6052d6619980bc2b215732b88f64d58182b13ffd
Krishna Mudragada <mudragadak@ae.com>
2019-08-08 00:44:38 -0400

    commit from local

---
+++ Problems/p002.py
@@ -0,0 +1,18 @@
+# Print even fibonacci below a number
+
+
+def sumEvenFibonacci(limit):
+	startNum = 1
+	nextNum = 2
+	sumOfEvens = 0
+	while startNum <= limit:
+		if startNum%2 == 0:
+			sumOfEvens += startNum
+
+		startNum, nextNum = nextNum, startNum + nextNum
+
+	return sumOfEvens
+
+if __name__ == "__main__":
+	limit = input("Enter a limit:")
+	print(sumEvenFibonacci(int(limit)))
