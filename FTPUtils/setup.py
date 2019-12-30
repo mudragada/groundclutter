6772a9f5305a2d47fa2162e9fbdbf3a4148d3d49
mudragada <chaitanyamudragada@gmail.com>
2019-12-29 23:40:35 -0500

    Update setup.py

--- FTPUtils/setup.py
+++ FTPUtils/setup.py
@@ -1,9 +1,9 @@
 __author__ = 'Krishna Mudragada'
 from setuptools import setup
-setup(name='DataFeedsTester',
+setup(name='FeedTester',
       version='0.1',
       description='Test your Data feeds for validity',
-      url='http://github.com/mudragada/DataFeeds',
+      url='http://github.com/mudragada/Feeds',
       author='Krishna Mudragada',
       license='MIT',
       install_requires = ['logging', 're'])
